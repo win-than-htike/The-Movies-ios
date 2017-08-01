cce10d750be5564e9dc24e0153b4cdd50ead5f67
winnthannhtike <winnthannhtike@gmail.com>
2017-08-01 23:18:16 +0630

    Initial Commit

---
+++ The Movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Movie
+//
+//  Created by Win Than Htike on 8/1/17.
+//  Copyright © 2017 Win Than Htike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
